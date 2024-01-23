330d12c2eef57578f5350a0c1f13bea75b437c1d
cly <297758225@qq.com>
2024-01-23 14:31:17 +0800

    再次修改时间

--- article/models.py
+++ article/models.py
@@ -9,7 +9,7 @@ from PIL import Image
 class ArticleColumn(models.Model):
     objects = models.Manager()
     title = models.CharField(max_length=100, blank=True)
-    created = models.DateTimeField(default=timezone.now())
+    created = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.title
@@ -21,7 +21,7 @@ class ArticlePost(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     body = models.TextField()
-    created = models.DateTimeField(default=timezone.now())
+    created = models.DateTimeField(default=timezone.now)
     updated = models.DateTimeField(auto_now=True)
     total_views = models.PositiveIntegerField(default=0)
     tags = TaggableManager(blank=True)
